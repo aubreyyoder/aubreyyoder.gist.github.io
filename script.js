238c0fd8b3cef4c7bd1451e1dab5f524f102a389
Aubrey Yoder <Aubreyyoder@gmail.com>
2019-02-07 14:51:54 -0800

    added empty js functions

---
+++ script.js
@@ -0,0 +1,35 @@
+function aboutMeScroll() {
+//when tab in nav is clicked, scroll to #bio
+}
+
+function projectsScroll() {
+//when tab in nav bar is clicked, scroll to .projects
+}
+
+function contactScroll() {
+//when tab in nav is clicked, scroll to footer
+}
+
+function email() {
+//when tab in footer is clicked, create seperate window with email addressed to
+// 'aubreyyoder@gmail.com'
+}
+
+function github() {
+//when tab in footer is clicked, take to https://github.com/aubreyyoder/
+}
+
+function linkedin() {
+//when tab in footer is clicked, take to linkedin page
+}
+
+function handleAllEvents() {
+    $(aboutMeScroll);
+    $(projectScroll);
+    $(contactScroll);
+    $(email);
+    $(github);
+    $(linkedin);
+}
+
+$(handleAllEvents);
