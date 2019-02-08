3b08e20730fc599b45e334eff6397f2e41273508
Aubrey Yoder <Aubreyyoder@gmail.com>
2019-02-08 13:52:12 -0800

    fixing bug js

--- script.js
+++ script.js
@@ -3,7 +3,7 @@ function scrollToBio() {
     //when tab in nav is clicked, scroll to section
     $('.js-btn-about').click(() => {
         $('html, body').animate({
-            scrollTop: $('.js-section-projects').offset().top
+            scrollTop: $('.js-section-about').offset().top
         }, 500);
     });
 }
@@ -12,7 +12,7 @@ function scrollToProjects() {
     //when tab in nav is clicked, scroll to section
     $('.js-btn-projects').click(() => {
         $('html, body').animate({
-            scrollTop: $('.js-section-about').offset().top
+            scrollTop: $('.js-section-projects').offset().top
         }, 500);
     });
 }
